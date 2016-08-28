ffc77b414ef6c2f2c6e7d3311c01e1b963ba77f8
liyl <295697141@qq.com>
2016-08-28 23:13:33 +0800

    删除dist

---
+++ build/release.js
@@ -0,0 +1,28 @@
+// https://github.com/shelljs/shelljs
+require('shelljs/global')
+env.NODE_ENV = 'production'
+
+var path = require('path')
+var config = require('../config')
+var ora = require('ora')
+var webpack = require('webpack')
+var webpackConfig = require('./webpack.release.conf')
+
+var spinner = ora('release for production...')
+spinner.start()
+
+var assetsPath = path.join(config.release.assetsRoot, config.release.assetsSubDirectory)
+rm('-rf', assetsPath)
+mkdir('-p', assetsPath)
+
+webpack(webpackConfig, function (err, stats) {
+  spinner.stop()
+  if (err) throw err
+  process.stdout.write(stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  }) + '\n')
+})
