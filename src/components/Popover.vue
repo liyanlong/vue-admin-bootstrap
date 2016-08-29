a95cd97eb903df78484c9af376bca94ec6b2489b
liyl <295697141@qq.com>
2016-08-29 17:29:28 +0800

    添加 popover 组件

---
+++ src/components/Popover.vue
@@ -0,0 +1,33 @@
+<template>
+  <span v-el:trigger>
+    <slot></slot>
+  </span>
+  <div v-el:popover :class="['popover', effect, placement, {
+        'in': inShow
+      }]" :style="{
+            display: displayShow ? 'block' : 'none',
+            left: position.left + 'px',
+            top: position.top + 'px'
+        }">
+    <div class="arrow"></div>
+    <h3 class="popover-title">
+      <slot name="popover-title">{{title}}</slot>
+    </h3>
+    <div class="popover-content">
+      <slot name="popover-content">{{{content}}}</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import PopoverMixin from './mixins/popoverMixins.js'
+export default {
+    mixins: [PopoverMixin],
+    props: {
+        trigger: {
+            type: String,
+            default: 'click'
+        }
+    }
+}
+</script>
